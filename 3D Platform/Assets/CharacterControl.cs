1b26ef35e2f0b878fb7b66a983d2b966e5666975
olivergado <ogado2022@elam.co.uk>
2022-11-16 10:59:11 +0000

    Working on movement

--- 3D Platform/Assets/CharacterControl.cs
+++ 3D Platform/Assets/CharacterControl.cs
@@ -12,9 +12,16 @@ public class CharacterControl : MonoBehaviour
     GameObject cam;
     Rigidbody myRigidbody;
 
+    bool isOnGround;
+    public GameObject groundChecker;
+    public LayerMask groundLayer;
+    public float jumpForce = 300.0f;
+
     // Start is called before the first frame update
     void Start()
     {
+        Cursor.lockState = CursorLockMode.Locked;
+
         cam = GameObject.Find("Main Camera");
         myRigidbody = GetComponent<Rigidbody>();
     }
@@ -22,7 +29,13 @@ public class CharacterControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //transform.position = transform.position + (transform.forward * Input.GetAxis("Vertical") * maxSpeed);
+        isOnGround = Physics.CheckSphere(groundChecker.transform.position, 0.1f, groundLayer);
+
+        if (isOnGround == true && Input.GetKeyDown(KeyCode.Space))
+        {
+            myRigidbody.AddForce(transform.up * jumpForce);
+        }
+      
 
         Vector3 newVelocity = transform.forward * Input.GetAxis("Vertical") * maxSpeed;
         myRigidbody.velocity = new Vector3(newVelocity.x, myRigidbody.velocity.y, newVelocity.z);
@@ -30,7 +43,7 @@ public class CharacterControl : MonoBehaviour
         rotation = rotation + Input.GetAxis("Mouse X") * rotationSpeed;
         transform.rotation = Quaternion.Euler(new Vector3(0.0f, rotation, 0.0f));
 
-        camRotation = camRotation + Input.GetAxis("Mouse Y") * camRotationSpeed;
+        camRotation = camRotation + -Input.GetAxis("Mouse Y") * camRotationSpeed;
         cam.transform.localRotation = Quaternion.Euler(new Vector3(camRotation, 0.0f, 0.0f));
     }
 }
