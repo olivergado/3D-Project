c78fca54e26e913f1218b8bb641de038a485768f
olivergado <ogado2022@elam.co.uk>
2022-12-01 12:44:40 +0000

    working on other scenes

--- 3D Platform/Assets/WinBox.cs
+++ 3D Platform/Assets/WinBox.cs
@@ -2,14 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WinhBox : MonoBehaviour
+public class WinBox : MonoBehaviour
 {
     // Start is called before the first frame update
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
-            UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+            UnityEngine.SceneManagement.SceneManager.LoadScene(2);
         }
     }
 
